b44b089b0e2c9b70966307df6445dc811ca7f4ce
Moisés Dorado <dorado.gumoi24@triana.salesianos.edu>
2025-02-18 14:29:32 +0100

    proyecto

---
+++ DoradoMoises_Ready2Ref/src/main/java/com/salesianos/triana/DoradoMoises_Ready2Ref/DoradoMoisesReady2RefApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.DoradoMoises_Ready2Ref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoradoMoisesReady2RefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoradoMoisesReady2RefApplication.class, args);
+	}
+
+}
---
+++ DoradoMoises_Ready2Ref/src/test/java/com/salesianos/triana/DoradoMoises_Ready2Ref/DoradoMoisesReady2RefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.DoradoMoises_Ready2Ref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoradoMoisesReady2RefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
