6b7c7a57c570ff0ed3c3c291aaa62f1b6ecc6a18
MoisésDorado <dorado.gumoi24@triana.salesianos.edu>
2025-02-20 21:02:54 +0100

    revisar parte del email

---
+++ DoradoMoises_Ready2Ref/src/main/java/com/salesianos/triana/DoradoMoises_Ready2Ref/service/UserService.java
@@ -0,0 +1,66 @@
+package com.salesianos.triana.DoradoMoises_Ready2Ref.service;
+
+import com.salesianos.triana.DoradoMoises_Ready2Ref.dto.CreateUserRequest;
+import com.salesianos.triana.DoradoMoises_Ready2Ref.error.ActivationExpiredException;
+import com.salesianos.triana.DoradoMoises_Ready2Ref.model.User;
+import com.salesianos.triana.DoradoMoises_Ready2Ref.model.UserRole;
+import com.salesianos.triana.DoradoMoises_Ready2Ref.repository.UserRepository;
+import com.salesianos.triana.DoradoMoises_Ready2Ref.util.SendGridMailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final SendGridMailSender mailSender;
+
+    @Value("${activation.duration}")
+    private int activationDuration;
+
+    public User createUser(CreateUserRequest createUserRequest) {
+        User user = User.builder()
+                .username(createUserRequest.username())
+                .password(passwordEncoder.encode(createUserRequest.password()))
+                .email(createUserRequest.email())
+                .roles(Set.of(UserRole.USER))
+                .activationToken(generateRandomActivationCode())
+                .build();
+
+        try {
+            mailSender.sendMail(createUserRequest.email(), "Activación de cuenta", user.getActivationToken());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Error al enviar el email de activación");
+        }
+
+        return userRepository.save(user);
+    }
+
+    public String generateRandomActivationCode() {
+        return UUID.randomUUID().toString();
+    }
+
+    public User activateAccount(String token) {
+
+        return userRepository.findByActivationToken(token)
+                .filter(user -> ChronoUnit.MINUTES.between(Instant.now(), user.getCreatedAt()) - activationDuration < 0)
+                .map(user -> {
+                    user.setEnabled(true);
+                    user.setActivationToken(null);
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new ActivationExpiredException("El código de activación no existe o ha caducado"));
+    }
+
+}
--- DoradoMoises_Ready2Ref/src/main/java/com/salesianos/triana/DoradoMoises_Ready2Ref/util/SendGridMailSender.java
+++ DoradoMoises_Ready2Ref/src/main/java/com/salesianos/triana/DoradoMoises_Ready2Ref/util/SendGridMailSender.java
@@ -1,5 +1,12 @@
 package com.salesianos.triana.DoradoMoises_Ready2Ref.util;
 
+import com.resend.services.emails.model.Email;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -17,7 +24,25 @@ public class SendGridMailSender {
 
     @Async
     public void sendMail(String to, String subject, String message) throws IOException {
-        log.info("Enviando email a " + to + " con asunto " + subject);
+        Email from = new Email("dorado.gumoi24@triana.salesianos.edu");
+        Email recipient = new Email(to);
+        Content content = new Content("text/html", message);
+        Mail mail = new Mail(from, subject, recipient, content);
 
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            System.out.println("Response Code: " + response.getStatusCode());
+            System.out.println("Response Body: " + response.getBody());
+
+        } catch (IOException ex) {
+            throw new IOException("Error al enviar email con SendGrid", ex);
+        }
     }
 }
